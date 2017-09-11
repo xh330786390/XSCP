7a74e12c06105725c10a554fd21b8d93020d5d40
xh330786390 <330786390@qq.com>
2017-09-11 15:38:48 +0800

    添加million model

--- XSCP.Common/Model/MillionJsonModel.cs
+++ XSCP.Common/Model/MillionJsonModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +7,36 @@ using System.Threading.Tasks;
 
 namespace XSCP.Common.Model
 {
-    //{"historyBall":{"1":{"code":"0 0 9 5 9","issue":"20170907-0080"},"2":{"code":"3 7 6 9 9","issue":"20170907-0079"},"3":{"code":"7 2 0 4 9","issue":"20170907-0078"},"4":{"code":"1 8 6 4 7","issue":"20170907-0077"},"5":{"code":"4 8 6 6 9","issue":"20170907-0076"}},"lastballsTrans":["\u524d\u4e09\uff1a\u7ec4\u4e09","\u4e2d\u4e09\uff1a\u7ec4\u4e09","\u540e\u4e09\uff1a\u7ec4\u516d"],"isSuccess":1,"period":"20170907-0081","ball":"1,5,5,1,4"}
-   public  class MillionJsonModel
+    //{"historyBall":{"1":{"code":"4 8 1 1 9","issue":"20170907-0399"},"2":{"code":"9 6 3 0 0","issue":"20170907-0398"},"3":{"code":"4 9 5 5 2","issue":"20170907-0397"},"4":{"code":"5 7 4 6 6","issue":"20170907-0396"},"5":{"code":"2 6 7 8 2","issue":"20170907-0395"}},"lastballsTrans":["\u524d\u4e09\uff1a\u7ec4\u516d","\u4e2d\u4e09\uff1a\u7ec4\u516d","\u540e\u4e09\uff1a\u7ec4\u516d"],"isSuccess":1,"period":"20170907-0400","ball":"9,3,8,6,1"}
+    public class MillionJsonModel
     {
-        public string historyBall{get;set;}
+        public ListWiner historyBall { get; set; }
 
-       public string a1{get;set;}
+        public string period { get; set; }
+        public string ball { get; set; }
+    }
+
+    public class ListWiner
+    {
+        [JsonProperty("1")]
+        public MillonWinner period1 { get; set; }
+
+        [JsonProperty("2")]
+        public MillonWinner period2 { get; set; }
+
+        [JsonProperty("3")]
+        public MillonWinner period3 { get; set; }
+
+        [JsonProperty("4")]
+        public MillonWinner period4 { get; set; }
+
+        [JsonProperty("5")]
+        public MillonWinner period5 { get; set; }
+    }
+
+    public class MillonWinner
+    {
+        public string code { get; set; }
+        public string issue { get; set; }
     }
 }
